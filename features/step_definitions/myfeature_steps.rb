cfab6d056c3a7676491f4eed16942624b079acd1
Hussein <tejanahmedhu@gmail.com>
2016-11-22 00:21:13 +0000

    first commit message received

---
+++ features/step_definitions/myfeature_steps.rb
@@ -0,0 +1,10 @@
+Given(/^I have (\d+) in my account$/) do |balance|
+end
+
+Given(/^I withraw (\d+)$/) do |withdrawal|
+
+end
+
+Then(/^I should have (\d+) left in my account$/) do |remaining|
+
+end
